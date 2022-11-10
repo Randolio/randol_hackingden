b6d18f72c892dc071e076b2f6f1e5febf0577caf
Randolio <103154494+Randolio@users.noreply.github.com>
2022-11-10 07:20:37 -0800

    Add files via upload

---
+++ randol_hackingden/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Randolio'
+description 'Multi Hacks Practice'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	'client/*.lua',
+}
+
+lua54 'yes'
